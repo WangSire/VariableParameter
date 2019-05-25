94131ff1103a1ed0788e1aca56c20cdb707758d8
Siri <75486736@qq.com>
2019-05-25 16:51:19 +0800

    no message

---
+++ 双向数据绑定/IQDataBinding/IQDataBinding.h
@@ -0,0 +1,24 @@
+//
+//  IQDataBinding.h
+//  IQDataBinding
+//
+//  Created by lobster on 2019/5/2.
+//  Copyright © 2019 lobster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "NSObject+IQDataBinding.h"
+
+//! Project version number for IQDataBinding.
+FOUNDATION_EXPORT double IQDataBindingVersionNumber;
+
+//! Project version string for IQDataBinding.
+FOUNDATION_EXPORT const unsigned char IQDataBindingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IQDataBinding/PublicHeader.h>
+/**
+ TODO LIST:
+ 1.同一view绑定多个model的情况。
+ 2.view变动->更新viewModel。
+ */
+
